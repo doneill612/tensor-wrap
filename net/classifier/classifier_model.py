c2eec8cea9a977ec609f4499fc623fb7d8703499
David O'Neill <doneill612@gmail.com>
2018-08-21 10:43:16 +0200

    restructure, new abstraction style, initial classifier model upload

---
+++ net/classifier/classifier_model.py
@@ -0,0 +1,40 @@
+import net.model
+import classifier_config
+import classifier_graph
+import classifier_runner
+
+class Classifier(net.model.Model):
+
+    def __init__(self, name: str, config: 'ClassifierConfig') -> None:
+        super(Classifier, self).__init__(name)
+        if not isinstance(config, classifier_config.ClassifierConfig):
+            raise ValueError('The config parameter must
+                             'be a ClassifierConfig object')
+        self._config = config
+
+    def build_computation_graph(self, mode: str) -> None:
+        self._graph = classifier_graph.build(mode, self.classifier_config)
+
+    def train(self) -> None:
+        if self._graph is None:
+            raise RuntimeError('Classifier model object {id} has None-type '
+                               'computation graph. Must first call '
+                               '#build_computation_graph.'.format(id=self))
+        if self._graph.mode is not 'train':
+            raise RuntimeError('Classifier graph {id} contains use-type ops. '
+                               'Must reconstruct the computation graph in '
+                               '\'train\' mode.'.format(id=self._graph))
+        classifier_runner.train(self._graph, self._session,
+                               self._name, self._config)
+
+    def train(self) -> None:
+        if self._graph is None:
+           raise RuntimeError('Classifier model object {id} has None-type '
+                              'computation graph. Must first call '
+                              '#build_computation_graph.'.format(id=self))
+        if self._graph.mode is not 'use':
+           raise RuntimeError('Classifier graph {id} contains train-type ops. '
+                              'Must reconstruct the computation graph in '
+                              '\'use\' mode.'.format(id=self._graph))
+        classifier_runner.test(self._graph, self._session,
+                               self._name, self._config)
---
+++ net/model.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import abc
+
+class Model(object, metaclass=abc.ABCMeta):
+    """
+    A TensorFlow-based neural network class model.
+    """
+    def __init__(self, name):
+         """
+        params:
+            `name`      : the model name - used for saving purposes
+            `session`   : the TensorFlow session to run the computation graph
+            `graph`     : the TensorFlow computation graph
+        """
+        self._session = None
+        self._graph = None
+        self._name = name
+
+    @abc.abstractmethod
+    def build_computation_graph(self, mode):
+        """
+        Constructs the TensorFlow computation graph.
+        params:
+            mode : the graph building mode - either 'train' or 'use'
+        returns:
+            a `tf.Graph()` object
+        """
+        pass
+
+    @abc.abstractmethod
+    def train(self):
+        """
+        Trains a network model.
+        """
+        pass
+
+    @abc.abstractmethod
+    def test(self):
+        """
+        Runs testing on this model's trained network state.
+        """
+        pass
